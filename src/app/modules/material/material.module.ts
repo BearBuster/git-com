d7500d4b86502cd8d20d8f73c2f5ba7345306146
Dumitru Frunza <crme009@cedmd.ced.it>
2023-02-01 16:05:17 +0200

    Spinner add

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule} from "@angular/material/button";
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const MaterialComponents = [
   MatButtonModule,
   MatGridListModule,
-  MatCardModule
+  MatCardModule,
+  MatProgressSpinnerModule
 ]
 @NgModule({
   imports: [
