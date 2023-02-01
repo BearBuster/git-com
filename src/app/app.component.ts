c6ef0ff366fcb715f4779e06cc78ae7d4d85d219
Marina Burlacu <crme008@cedmd.ced.it>
2023-02-01 15:49:33 +0000

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(){
+    localStorage.setItem("profileKey","4")
+  }
   title = 'git-com';
 }
