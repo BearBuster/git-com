0eaf94f19e2cbcf3b51d9f59e742358aec2831e0
Dumitru Frunza <crme009@cedmd.ced.it>
2023-02-01 17:50:26 +0200

    alert

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -12,6 +12,8 @@ export class LoginComponent {
   constructor(private router: Router, private auth: Auth) { }
 
   onSubmit(f: NgForm){
-    this.auth.login(f.value.username, f.value.password)
+    if(!this.auth.login(f.value.username, f.value.password)){
+      alert("Login or Password incorrect !")
+    }
   }
 }
